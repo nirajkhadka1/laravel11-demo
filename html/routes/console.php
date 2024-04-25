66175b14ccee58544afb413efbf89c16d4a62a1b
Niraj Khadka <niraj.khadka@web.digital>
2024-04-25 12:27:01 +0545

    command line validation

--- html/routes/console.php
+++ html/routes/console.php
@@ -3,6 +3,20 @@
 use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
 
+use function Laravel\Prompts\text;
+
 Artisan::command('inspire', function () {
     $this->comment(Inspiring::quote());
 })->purpose('Display an inspiring quote')->hourly();
+
+Artisan::command('validation', function(){
+    $name = text(
+        label: 'What is your name?',
+        validate: fn (string $value) => match (true) {
+            strlen($value) < 3 => 'The name must be at least 3 characters.',
+            strlen($value) > 6 => 'The name must not exceed 6 characters.',
+            default => null
+        }
+    );
+    
+});
