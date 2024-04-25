72de581b8de17bc59765b9957e91b4a183948506
Niraj Khadka <niraj.khadka@web.digital>
2024-04-25 14:19:06 +0545

    encryption key rotation

--- html/routes/web.php
+++ html/routes/web.php
@@ -1,7 +1,34 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Crypt;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+Route::get('encrypt', function(Request $request){
+    $phrase = $request->data;
+    return Crypt::encryptString($phrase);
+});
+
+Route::get('decrypt', function(Request $request){
+    $phrase = $request->data;
+    return Crypt::decryptString($phrase);
+});
+
+
+
+require __DIR__.'/auth.php';
